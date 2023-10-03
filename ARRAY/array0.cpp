2b77d5f07d000a5a66b0f60f9cd2c0d3c53af8ee
P01Sensai <pramanshuprajapati@gmil.com>
2023-10-04 00:00:39 +0530

    first Commit

---
+++ ARRAY/array0.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int arr[10] = {1,2,3,4,5,6};
+    int arrr[1];
+    // int n = 3;
+    // int brr[cin>>n;];
+    //cout<<&arr<<" "<<arr<<endl;
+    cout<<arrr[90];
+}
