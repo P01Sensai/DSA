a20b3bcae4e78d394fd08669a5bc03ba9ae0b7b9
P01Sensai <pramanshuprajapati@gmil.com>
2023-10-04 00:40:20 +0530

    code created

--- Searching_Sorting/linearsearch.cpp
+++ Searching_Sorting/linearsearch.cpp
@@ -7,14 +7,23 @@ int main(){
     cin>>n;
     int b;
     vector <int> arr;
-    for (int i =0 ;i<=n;i++){
+    for (int i =0 ;i<n;i++){
         cin>>b;
         arr.push_back(b);
 
     }
-    cout<< "output" ;
+    cout<< "output" <<endl; 
     for(auto print : arr){
         cout<<print<<" ";
+    }
+    cout<<endl;
+    cout<<" whats your target element = ";
+    int target;
+    cin>>target;
+    for(int i = 0;i<n;i++){
+        if(arr[i] == target) cout<<i<<endl;
+        
+
     }
 
 
